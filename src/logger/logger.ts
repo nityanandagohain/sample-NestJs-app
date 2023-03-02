e68bbd079bab81818cfe258c4469ca04df564ae3
nityanandagohain <nityanandagohain@gmail.com>
2023-03-02 17:01:36 +0530

    file logger added

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,20 +1,38 @@
-import Pino, { Logger } from 'pino';
+import Pino, { Logger, multistream, transport } from 'pino';
 import { LoggerOptions, destination } from 'pino';
 import { trace, context } from '@opentelemetry/api';
+import * as fs from 'fs';
 
 export const loggerOptions: LoggerOptions = {
     level: 'info',
     transport: {
-        target: 'pino-pretty',
+        targets:[{
+            level:"info",
+            target: 'pino-pretty',
         options: {
             colorize: true,
             ignore: "pid,hostname,context,req,res,responseTime,trace_id,span_id,trace_flags",
-            messageFormat: '{levelLabel} - {pid} - url:{req.url} - traceId:{trace_id} - spanId:{span_id} {msg}'
+            messageFormat: '\{"trace_id":"{trace_id}","spanId":"{span_id}","message":"{msg}"\}'
         }
+        },
+        {
+            level:"info",
+            target: 'pino-pretty',
+            options: {
+                colorize: false,
+                ignore: "pid,hostname,context,req,res,responseTime,trace_id,span_id,trace_flags",
+                messageFormat: '\{"traceId":"{trace_id}","spanId":"{span_id}","message":"{msg}","serviceName": "sampleNestJsApp"\}',
+                destination: '/Users/nityanandagohain/projects/signoz/deploy/docker/clickhouse-setup/log.log'
+            }
+        },
+    
+        ],
+        
     }
 };
 
+
+
 export const logger: Logger = Pino(
     loggerOptions,
-    destination(process.env.LOG_FILE_NAME),
 );
